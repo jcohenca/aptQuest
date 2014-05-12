241c580a3049f4b9cd213985e5a34eb5e8159701
jcohenca <jcohenca@gmail.com>
2014-05-11 21:19:31 -0400

    test

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,15 +11,21 @@ class SearchesController < ApplicationController
 
   def new
 
+
+
     if current_user
       @new_search = Search.new(input: params[:auto], user_id: current_user.id)
       @new_search.save
 
-      redirect_to :root
-
     else
       redirect_to :root
 
+    end
+
+
+    respond_to do |format|
+      format.html { redirect_to '/' }
+      format.js {  }
     end
 
   end
@@ -31,7 +37,6 @@ class SearchesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to '/' }
       format.js {  }
-      format.json { render json: search.to_json }
     end
 
   
