eb126650f7d7d315be57e56eb69fc12038bb9a22
jcohenca <jcohenca@gmail.com>
2014-05-12 22:54:39 -0400

    yelp searches now pulling correctly

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,16 @@
 class WelcomeController < ApplicationController
 
   def index
+    # @gym = Search.yelp_search('gym')
+    # @groceries = Search.yelp_search('groceries')
+    # @alcohol = Search.yelp_search('alcohol')
+    # @haircut = Search.yelp_search('salon')
+    # @cafe = Search.yelp_search('cafe')
+    # @library = Search.yelp_search('library')
+    # @bars = Search.yelp_search('bar')
+    # @movies = Search.yelp_search('movie')
+    # @park = Search.yelp_search('park')
   end
-
-
-
-
-def render_map
-  render :partial => 'map'
-end
 
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,25 +1,27 @@
+require 'pry'
 class Search < ActiveRecord::Base
   belongs_to :user
 
+  Yelp.configure(:yws_id          => 'wI-IeFEyCH7wtK7pnjjN3Q',
+                 :consumer_key    => 'yVTzyc9fbaCu1pNipJkRzA',
+                 :consumer_secret => 'i24PY51TjhOe3_aT8XwmNA7_vJ0',
+                 :token           => '3ACW86hcthAxJslUG_HqRDZDazDZTVth',
+                 :token_secret    => 'OAEAtQPZnhna33YkLPzsR43hpJU')
 
 
+  def self.yelp_search(type)
 
-  def self.yelp_search
+    client = Yelp::Client.new
+    request = Yelp::V2::Search::Request::GeoPoint.new(
+                :term            =>  type,
+                :latitude        => '40.76741260000001',  
+                :longitude       => '-73.98381810000001')
 
-  client = Yelp::Client.new
-  
-  request = Yelp::V2::Search::Request::GeoPoint.new(
-              :latitude        => '39.62994399999999',
-              :longitude       => '-78.746465',
-              :yws_id          => 'wI-IeFEyCH7wtK7pnjjN3Q',
-              :consumer_key    => 'yVTzyc9fbaCu1pNipJkRzA',
-              :consumer_secret => 'i24PY51TjhOe3_aT8XwmNA7_vJ0',
-              :token           => '3ACW86hcthAxJslUG_HqRDZDazDZTVth',
-              :token_secret    => 'OAEAtQPZnhna33YkLPzsR43hpJU')
+    response = client.search(request)
+    final_return = response["businesses"].sort_by {|b| b["distance"]}.first["name"]
 
-  @response = client.search(request)
-
+  end 
   end
 
+ 
 
-end
